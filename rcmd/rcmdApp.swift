fc71582c9d8136a476216bbaecd5616a5e8b1eb5
Hang Duong <irene.duong799@gmail.com>
2023-05-08 10:23:56 -0700

    Initial Commit

---
+++ rcmd/rcmdApp.swift
@@ -0,0 +1,17 @@
+//
+//  rcmdApp.swift
+//  rcmd
+//
+//  Created by Hang Duong on 2023-05-08.
+//
+
+import SwiftUI
+
+@main
+struct rcmdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
